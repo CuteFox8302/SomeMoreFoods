6c2ab3209b1fea704bd767f50eedbb5f3af1885c
CuteFox8302 <111240985+CuteFox8302@users.noreply.github.com>
2023-01-07 15:26:49 -0500

    Adds sap texture. Adds Mod Item Group. Adds Sap model file. Adds translations. Adds Sap item.

--- src/main/java/net/cutefox/somemorefoods/item/ModItemGroup.java
+++ src/main/java/net/cutefox/somemorefoods/item/ModItemGroup.java
@@ -1,5 +1,12 @@
 package net.cutefox.somemorefoods.item;
 
-public class ModItemGroup {
+import net.cutefox.somemorefoods.SomeMoreFoods;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
+public class ModItemGroup {
+    public static final ItemGroup SOME_MORE_FOODS = FabricItemGroupBuilder.build(
+            new Identifier(SomeMoreFoods.MOD_ID, "some_more_foods"), () -> new ItemStack(ModItems.SAP));
 }
--- src/main/java/net/cutefox/somemorefoods/item/ModItems.java
+++ src/main/java/net/cutefox/somemorefoods/item/ModItems.java
@@ -1,8 +1,19 @@
 package net.cutefox.somemorefoods.item;
 
 import net.cutefox.somemorefoods.SomeMoreFoods;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ModItems {
+
+    public static final Item SAP = registerItem("sap",
+            new Item(new FabricItemSettings().food(ModFoodComponents.SAP).group(ModItemGroup.SOME_MORE_FOODS)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(SomeMoreFoods.MOD_ID, name), item);
+    }
 
     public static void registerModItems() {
         SomeMoreFoods.LOGGER.debug("Registering Mod Items for " + SomeMoreFoods.MOD_ID);
